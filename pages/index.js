d6130d2d02b3727fbab02021104a86236c3bd3f9
Tales Montenegro <montenegro.tales@gmail.com>
2020-10-05 17:56:08 -0300

    feature(integration): Build backend integration

--- pages/index.js
+++ pages/index.js
@@ -38,6 +38,10 @@ export default function Home() {
   ];
 
   const [products, setProducts] = useState([]);
+  const [taxedProducts, setTaxedProducts] = useState([]);
+
+  const [showTaxedProduct, setShowTaxedProduct] = useState(false);
+  const [showProduct, setShowProduct] = useState(true);
 
   const [imported, setImported] = useState("");
 
@@ -69,6 +73,9 @@ export default function Home() {
 
   const handleNewProduct = (event) => {
     event.preventDefault();
+    setShowProduct(true);
+    setShowTaxedProduct(false);
+    setTaxedProducts([]);
     setProducts([
       ...products,
       {
@@ -81,11 +88,34 @@ export default function Home() {
     ]);
   };
 
-  const handleProducts = (event) => {
+  async function handleProducts (event){
     event.preventDefault();
 
-    console.log("products", products);
-  };
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(products)
+    };
+    const response = await fetch('http://taxcalculator-env-1.eba-dfnvacpx.us-east-1.elasticbeanstalk.com/checkout', requestOptions);
+    const taxedProducts = await response.json();
+    setTaxedProducts(taxedProducts);
+    setProducts([]);
+    
+    setShowProduct(false);
+    setShowTaxedProduct(true);
+  }
+
+  const resetProducts = () => {
+    setShowProduct(true);
+    setShowTaxedProduct(false);
+    setProducts([]);
+    setTaxedProducts([]);
+    setImported("");
+    setDescription("");
+    setCategory("");
+    setAmount("");
+    setPrice("");
+  }
 
   return (
     <div className={styles.container}>
@@ -114,116 +144,159 @@ export default function Home() {
 
         <div className={styles.taxCalculatorContainer}>
           <div className={styles.taxCalculatorForm}>
-            <form
-              className={styles.taxCalculatorFormContent}
-              onSubmit={handleNewProduct}
-            >
-              <div className={styles.taxCalculatorFormSection}>
-                <TextField
-                  id="imported"
-                  select
-                  label="Imported product?"
-                  value={imported}
-                  onChange={handleImported}
-                  variant="outlined"
-                  className={styles.taxCalculatorFormImported}
-                  required
-                >
-                  {importedOptions.map((option) => (
-                    <option key={option.value} value={option.value}>
-                      {option.label}
-                    </option>
-                  ))}
-                </TextField>
-              </div>
-              <div className={styles.taxCalculatorFormSection}>
-                <TextField
-                  id="description"
-                  label="Description"
-                  value={description}
-                  onChange={handleDescription}
-                  variant="outlined"
-                  className={styles.taxCalculatorFormItem}
-                  required
-                />
-                <TextField
-                  id="category"
-                  select
-                  label="Category"
-                  value={category}
-                  onChange={handleCategory}
-                  variant="outlined"
-                  className={styles.taxCalculatorFormItem}
-                  required
-                >
-                  {categoriesOptions.map((option) => (
-                    <option key={option.value} value={option.value}>
-                      {option.label}
-                    </option>
-                  ))}
-                </TextField>
-              </div>
-              <div className={styles.taxCalculatorFormSection}>
-                <TextField
-                  type="number"
-                  id="amount"
-                  label="Amount"
-                  value={amount}
-                  onChange={handleAmount}
-                  variant="outlined"
-                  className={styles.taxCalculatorFormItem}
-                  required
-                />
-                <TextField
-                  type="number"
-                  id="price"
-                  label="Price"
-                  value={price}
-                  onChange={handlePrice}
-                  variant="outlined"
-                  className={styles.taxCalculatorFormItem}
-                  required
-                />
-              </div>
-              <div className={styles.taxCalculatorFormSection}>
-                <Button
-                  type="submit"
-                  variant="contained"
-                  className={styles.taxCalculatorFormItem}
-                >
-                  Adicionar produto
-                </Button>
-                <Button
-                  type="button"
-                  variant="contained"
-                  color="primary"
-                  onClick={handleProducts}
-                  className={styles.taxCalculatorFormItem}
-                  disabled={products.length == 0}
-                >
-                  Gerar Recibo
-                </Button>
-              </div>
-            </form>
+            {showTaxedProduct && (
+                <div className={styles.taxCalculatorFormContent}>
+                  <div className={styles.taxCalculatorFormSection}>
+                  <Button
+                    variant="contained"
+                    onClick={resetProducts}
+                  >
+                    Make a new order
+                  </Button>
+                  </div>
+                </div>
+            )}
+            {showProduct && (
+              <form
+                className={styles.taxCalculatorFormContent}
+                onSubmit={handleNewProduct}
+              >
+                <h4>You can only have a receipt if you have added at least one product</h4>
+                <div className={styles.taxCalculatorFormSection}>
+                  <TextField
+                    id="imported"
+                    select
+                    label="Imported product?"
+                    value={imported}
+                    onChange={handleImported}
+                    variant="outlined"
+                    className={styles.taxCalculatorFormImported}
+                    required
+                  >
+                    {importedOptions.map((option) => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                  </TextField>
+                </div>
+                <div className={styles.taxCalculatorFormSection}>
+                  <TextField
+                    id="description"
+                    label="Description"
+                    value={description}
+                    onChange={handleDescription}
+                    variant="outlined"
+                    className={styles.taxCalculatorFormItem}
+                    required
+                  />
+                  <TextField
+                    id="category"
+                    select
+                    label="Category"
+                    value={category}
+                    onChange={handleCategory}
+                    variant="outlined"
+                    className={styles.taxCalculatorFormItem}
+                    required
+                  >
+                    {categoriesOptions.map((option) => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                  </TextField>
+                </div>
+                <div className={styles.taxCalculatorFormSection}>
+                  <TextField
+                    type="number"
+                    id="amount"
+                    label="Amount"
+                    value={amount}
+                    onChange={handleAmount}
+                    variant="outlined"
+                    className={styles.taxCalculatorFormItem}
+                    required
+                  />
+                  <TextField
+                    type="number"
+                    id="price"
+                    label="Price"
+                    value={price}
+                    onChange={handlePrice}
+                    variant="outlined"
+                    className={styles.taxCalculatorFormItem}
+                    required
+                  />
+                </div>
+                <div className={styles.taxCalculatorFormSection}>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    className={styles.taxCalculatorFormItem}
+                  >
+                    Add product
+                  </Button>
+                  <Button
+                    type="button"
+                    variant="contained"
+                    color="primary"
+                    onClick={handleProducts}
+                    className={styles.taxCalculatorFormItem}
+                    disabled={products.length == 0}
+                  >
+                    Make Receipt
+                  </Button>
+                </div>
+              </form>
+            )}
           </div>
-          {!!products.length && (
+          {showProduct && (
             <div className={styles.taxCalculatorProducts}>
               <div className={styles.productsContainer}>
                 {products.map((product, index) => (
-                  <a key={index} href="" className={styles.card}>
+                  <a key={index} className={styles.card}>
                     <h3>{product.description}</h3>
                     <div className={styles.productContent}>
-                      <p>Category: {product.category}</p>
-                      <p>Amount: {product.amount}</p>
+                      <p><b>Category:</b> {product.category}</p>
+                      <p><b>Amount:</b> {product.amount}</p>
                     </div>
                     <div className={styles.productContent}>
                       <p>Price: {product.price}</p>
-                      <p>Imported: {product.imported}</p>
+                      <p>Imported: {product.imported ? "Yes" : "No"}</p>
                     </div>
                   </a>
                 ))}
               </div>
             </div>
+          )}
+          {showTaxedProduct && (
+            <div className={styles.taxCalculatorProducts}>
+            <div className={styles.productsContainer}>
+              <>
+                <a className={styles.card}>
+                  <p>Total Price: {taxedProducts.total}</p>
+                </a>
+                <a className={styles.card}>
+                  <p>Total Taxes Paid: {taxedProducts.saleTax}</p>
+                </a>
+                
+                {taxedProducts.products.map((taxedProduct, index) => (
+                  <a key={index} className={styles.card}>
+                    <h3>{taxedProduct.description}</h3>
+                    <div className={styles.productContent}>
+                      <p>Category: {taxedProduct.category}</p>
+                      <p>Amount: {taxedProduct.amount}</p>
+                    </div>
+                    <div className={styles.productContent}>
+                      <p>Taxed Price: {taxedProduct.price}</p>
+                      <p>Imported: {taxedProduct.imported ? "Yes" : "No"} </p>
+                    </div>
+                  </a>
+                ))}
+              </>
+            </div>
+          </div>
           )}
         </div>
 

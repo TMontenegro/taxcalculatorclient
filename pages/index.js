3e3e019e17fb2e1bd4f6acc65f848f1c14dd9005
Tales Montenegro <montenegro.tales@gmail.com>
2020-10-05 11:37:09 -0300

    feature(index): Create mocked client basic structure

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,215 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { useState } from "react";
 
 export default function Home() {
+  // http://taxcalculator-env-1.eba-dfnvacpx.us-east-1.elasticbeanstalk.com/checkout
+
+  const importedOptions = [
+    {
+      value: true,
+      label: "Sim",
+    },
+    {
+      value: false,
+      label: "Não",
+    },
+  ];
+
+  const [imported, setImported] = React.useState("");
+
+  const handleImported = (event) => {
+    setImported(event.target.value);
+  };
+
+  const categoriesOptions = [
+    {
+      value: "book",
+      label: "Livros",
+    },
+    {
+      value: "food",
+      label: "Alimentos",
+    },
+    {
+      value: "medical",
+      label: "Remédios",
+    },
+    {
+      value: "none",
+      label: "Nenhuma das opções",
+    },
+  ];
+
+  const [categories, setCategories] = React.useState("");
+
+  const handleCategories = (event) => {
+    setCategories(event.target.value);
+  };
+
+  const [products, setProducts] = useState([
+    {
+      id: 1,
+      description: "Apple",
+      category: "Food",
+      amount: 1,
+      price: 10.0,
+      imported: "Sim",
+    },
+    {
+      id: 2,
+      description: "The Lord Of The Rings",
+      category: "Book",
+      amount: 1,
+      price: 47.5,
+      imported: "Não",
+    },
+  ]);
+  const [product, setProduct] = useState("");
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Tax Calculator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to{" "}
+          <a
+            href="https://github.com/TMontenegro/taxcalculator"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tax Calculator!
+          </a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>
+            A demo project for Liferay Interview
+          </code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.taxCalculatorContainer}>
+          <div className={styles.taxCalculatorForm}>
+            <form
+              className={styles.taxCalculatorFormContent}
+              noValidate
+              autoComplete="off"
+            >
+              <div className={styles.taxCalculatorFormSection}>
+                <TextField
+                  id="imported"
+                  select
+                  label="Produto importado?"
+                  value={imported}
+                  onChange={handleImported}
+                  variant="outlined"
+                  className={styles.taxCalculatorFormImported}
+                >
+                  {importedOptions.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </TextField>
+              </div>
+              <div className={styles.taxCalculatorFormSection}>
+                <TextField
+                  id="description"
+                  label="Descrição"
+                  variant="outlined"
+                  className={styles.taxCalculatorFormItem}
+                />
+                <TextField
+                  id="category"
+                  select
+                  label="Categoria"
+                  value={categories}
+                  onChange={handleCategories}
+                  variant="outlined"
+                  className={styles.taxCalculatorFormItem}
+                >
+                  {categoriesOptions.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </TextField>
+              </div>
+              <div className={styles.taxCalculatorFormSection}>
+                <TextField
+                  type="number"
+                  id="amount"
+                  label="Quantidade"
+                  variant="outlined"
+                  className={styles.taxCalculatorFormItem}
+                />
+                <TextField
+                  type="number"
+                  id="price"
+                  label="Preço"
+                  variant="outlined"
+                  className={styles.taxCalculatorFormItem}
+                />
+              </div>
+              <div className={styles.taxCalculatorFormSection}>
+                <Button
+                  type="button"
+                  onClick={() => setProducts([...products, product])}
+                  variant="contained"
+                  className={styles.taxCalculatorFormItem}
+                >
+                  Adicionar produto
+                </Button>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  className={styles.taxCalculatorFormItem}
+                >
+                  Gerar Recibo
+                </Button>
+              </div>
+            </form>
+          </div>
+          <div className={styles.taxCalculatorProducts}>
+            <div className={styles.productsContainer}>
+              {products.map((product) => (
+                <a key={product.key} href="" className={styles.card}>
+                  <h3>{product.description}</h3>
+                  <div className={styles.productContent}>
+                    <p>Category: {product.category}</p>
+                    <p>Amount: {product.amount}</p>
+                  </div>
+                  <div className={styles.productContent}>
+                    <p>Price: {product.price}</p>
+                    <p>Imported: {product.imported}</p>
+                  </div>
+                </a>
+              ))}
+            </div>
+          </div>
         </div>
+
+        <div className={styles.grid}></div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/TMontenegro"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Made by
+          <img src="/github.svg" alt="Github Logo" className={styles.logo} />
+          Tales Montenegro
         </a>
       </footer>
     </div>
-  )
+  );
 }
